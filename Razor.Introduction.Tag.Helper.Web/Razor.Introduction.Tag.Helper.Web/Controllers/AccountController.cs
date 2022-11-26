019dd9637da6a095a5de20ca6689e2d2703e8172
Cihat Girgin <cihatgirgin1@gmail.com>
2022-11-26 17:36:15 +0300

    Create AccountController.cs

---
+++ Razor.Introduction.Tag.Helper.Web/Razor.Introduction.Tag.Helper.Web/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Razor.Introduction.Tag.Helper.Web.DatabaseContexts;
+
+namespace Razor.Introduction.Tag.Helper.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserDatabaseContext _context;
+        public AccountController(UserDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string email,string password)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == email && x.Password == password);
+
+            if(user == null)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
