e8daed524eeecb481a2fc131472274e8e69bd809
Cihat Girgin <cihatgirgin1@gmail.com>
2022-11-26 17:03:28 +0300

    added database context and user model

---
+++ Razor.Introduction.Tag.Helper.Web/Razor.Introduction.Tag.Helper.Web/DatabaseContexts/UserDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Razor.Introduction.Tag.Helper.Web.Models;
+using System;
+
+namespace Razor.Introduction.Tag.Helper.Web.DatabaseContexts
+{
+    public class UserDatabaseContext: DbContext
+    {
+        public UserDatabaseContext(DbContextOptions<UserDatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Razor.Introduction.Tag.Helper.Web/Razor.Introduction.Tag.Helper.Web/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Razor.Introduction.Tag.Helper.Web.Models
+{
+    public class User
+    {
+        public string? Username { get; set; }
+        public string? Name { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
