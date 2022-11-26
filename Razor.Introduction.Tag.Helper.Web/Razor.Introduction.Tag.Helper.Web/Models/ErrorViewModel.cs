8943950a816f6af16036135f67c023edbf7ccdbd
Cihat Girgin <cihatgirgin1@gmail.com>
2022-11-26 16:32:33 +0300

    created project

---
+++ Razor.Introduction.Tag.Helper.Web/Razor.Introduction.Tag.Helper.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Razor.Introduction.Tag.Helper.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
